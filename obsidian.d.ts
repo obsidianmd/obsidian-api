9b69506127863102e1102cdeb4d14f523d08583e
lishid <lishid@gmail.com>
2021-02-01 11:04:04 -0500

    Update for v0.10.12.

--- obsidian.d.ts
+++ obsidian.d.ts
@@ -568,8 +568,18 @@ export interface DataAdapter {
      * @public
      */
     setMtime(normalizedPath: string, mtime: number): Promise<void>;
-
 }
+
+/**
+ * A standard debounce function.
+ *
+ * @param cb - The function to call.
+ * @param timeout - The timeout to wait.
+ * @param resetTimer - Whether to reset the timeout when the debouncer is called again.
+ * @returns a debounced function that takes the same parameter as the original function.
+ * @public
+ */
+export function debounce<T extends unknown[]>(cb: (...args: [...T]) => any, timeout?: number, resetTimer?: boolean): Debouncer<T>;
 
 /**
  * @public
@@ -1137,7 +1147,18 @@ export interface Loc {
  * removed from the app, look into {@link MarkdownPostProcessorContext#addChild}
  * @public
  */
-export type MarkdownPostProcessor = (el: HTMLElement, ctx: MarkdownPostProcessorContext) => Promise<any> | void;
+export interface MarkdownPostProcessor {
+    /**
+     * The processor function itself.
+     * @public
+     */
+    (el: HTMLElement, ctx: MarkdownPostProcessorContext): Promise<any> | void;
+    /**
+     * An optional integer sort order. Defaults to 0. Lower number runs before higher numbers.
+     * @public
+     */
+    sortOrder?: number;
+}
 
 /**
  * @public
@@ -1177,7 +1198,7 @@ export class MarkdownPreviewRenderer {
     /**
      * @public
      */
-    static registerPostProcessor(postProcessor: MarkdownPostProcessor): void;
+    static registerPostProcessor(postProcessor: MarkdownPostProcessor, sortOrder?: number): void;
     /**
      * @public
      */
@@ -1318,7 +1339,7 @@ export interface MarkdownSubView {
 /**
  * @public
  */
-export class MarkdownView extends EditableFileView {
+export class MarkdownView extends TextFileView {
 
     /**
      * @public
@@ -1332,12 +1353,6 @@ export class MarkdownView extends EditableFileView {
      * @public
      */
     currentMode: MarkdownSubView;
-
-    /**
-     * In memory data
-     * @public
-     */
-    data: string;
     
     /**
      * @public
@@ -1694,6 +1709,13 @@ export abstract class Plugin_2 extends Component {
      * @public
      */
     registerMarkdownPostProcessor(postProcessor: MarkdownPostProcessor): void;
+    /**
+     * Register a special post processor that handles fenced code given a language and a handler.
+     * This special post processor takes care of removing the <pre><code> and create a <div> that
+     * will be passed to your handler, and is expected to be filled with your custom elements.
+     * @public
+     */
+    registerMarkdownCodeBlockProcessor(language: string, handler: (source: string, el: HTMLElement, ctx: MarkdownPostProcessorContext) => Promise<any> | void): void;
     /**
      * Runs callback on all currently loaded instances of CodeMirror,
      * then registers the callback for all future CodeMirror instances.
@@ -2289,6 +2311,66 @@ export class TextComponent extends AbstractTextComponent<HTMLInputElement> {
 }
 
 /**
+ * This class implements a plaintext-based editable file view, which can be loaded and saved given an editor.
+ *
+ * Note that by default, this view only saves when it's closing. To implement auto-save, your editor should
+ * call `this.requestSave()` when the content is changed.
+ * @public
+ */
+export abstract class TextFileView extends EditableFileView {
+
+    /**
+     * In memory data
+     * @public
+     */
+    data: string;
+    /**
+     * Debounced save in 2 seconds from now
+     * @public
+     */
+    requestSave: () => void;
+    /**
+     * @public
+     */
+    constructor(leaf: WorkspaceLeaf);
+    
+    /**
+     * @public
+     */
+    onUnloadFile(file: TFile): Promise<void>;
+    /**
+     * @public
+     */
+    onLoadFile(file: TFile): Promise<void>;
+    /**
+     * @public
+     */
+    save(clear?: boolean): Promise<void>;
+
+    /**
+     * Gets the data from the editor. This will be called to save the editor contents to the file.
+     * @public
+     */
+    abstract getViewData(): string;
+    /**
+     * Set the data to the editor. This is used to load the file contents.
+     *
+     * If clear is set, then it means we're opening a completely different file.
+     * In that case, you should call clear(), or implement a slightly more efficient
+     * clearing mechanism given the new data to be set.
+     * @public
+     */
+    abstract setViewData(data: string, clear: boolean): void;
+    /**
+     * Clear the editor. This is usually called when we're about to open a completely
+     * different file, so it's best to clear any editor states like undo-redo history,
+     * and any caches/indexes associated with the previous file contents.
+     * @public
+     */
+    abstract clear(): void;
+}
+
+/**
  * @public
  */
 export class TFile extends TAbstractFile {
@@ -2320,7 +2402,7 @@ export class TFolder extends TAbstractFile {
      * @public
      */
     isRoot(): boolean;
-    
+
 }
 
 /**
@@ -2499,6 +2581,7 @@ export class Vault extends Events {
      * @public
      */
     on(name: 'rename', callback: (file: TAbstractFile, oldPath: string) => any, ctx?: any): EventRef;
+    
     /**
      * @public
      */
