2ccfad1a10c8da9066bf2c502f801b1a237f4ce4
lishid <lishid@gmail.com>
2023-09-07 00:16:33 -0400

    Add ViewStateResult.history. Fixes #129.

--- obsidian.d.ts
+++ obsidian.d.ts
@@ -4244,6 +4244,11 @@ export interface ViewState {
  * @public
  */
 export interface ViewStateResult {
+    /**
+     * Set this to true to indicate that there is a state change which should be recorded in the navigation history.
+     * @public
+     */
+    history: boolean;
 
 }
 
