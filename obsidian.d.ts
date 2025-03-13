385fae8a62ae2c1f29b36932cb43e6f7c995fdbc
Michael Naumov <mnaoumov@gmail.com>
2025-03-13 17:31:19 -0600

    Editor

--- obsidian.d.ts
+++ obsidian.d.ts
@@ -3719,81 +3719,414 @@ export abstract class EditableFileView extends FileView {
  */
 export abstract class Editor {
 
-    /** @public */
+    /**
+     * Get the editor instance
+     *
+     * @returns The editor instance
+     *
+     * @example
+     * ```ts
+     * console.log(editor.getDoc());
+     * ```
+     *
+     * @public
+     */
     getDoc(): this;
-    /** @public */
+    /**
+     * Refresh the editor
+     *
+     * @example
+     * ```ts
+     * editor.refresh();
+     * ```
+     *
+     * @public
+     */
     abstract refresh(): void;
-    /** @public */
+    /**
+     * Get the content of the editor
+     *
+     * @returns The content of the editor
+     *
+     * @example
+     * ```ts
+     * console.log(editor.getValue());
+     * ```
+     *
+     * @public
+     */
     abstract getValue(): string;
-    /** @public */
+    /**
+     * Set the content of the editor
+     *
+     * @param content - The content to set
+     *
+     * @example
+     * ```ts
+     * editor.setValue('foo');
+     * ```
+     *
+     * @public
+     */
     abstract setValue(content: string): void;
     /**
-     * Get the text at line (0-indexed)
+     * Get the text at line index (0-based)
+     *
+     * @param line - The line index
+     * @returns The text at the line
+     *
+     * @example
+     * ```ts
+     * console.log(editor.getLine(42));
+     * ```
+     *
      * @public
      */
     abstract getLine(line: number): string;
-    /** @public */
+    /**
+     * Set the text at line index (0-based)
+     *
+     * @param n - The line index
+     * @param text - The text to set
+     *
+     * @example
+     * ```ts
+     * editor.setLine(42, 'foo');
+     * ```
+     *
+     * @public
+     */
     setLine(n: number, text: string): void;
     /**
-     * Gets the number of lines in the document
+     * Gets the amount of lines in the document
+     *
+     * @returns The amount of lines in the document
+     *
+     * @example
+     * ```ts
+     * console.log(editor.lineCount());
+     * ```
+     *
      * @public
      */
     abstract lineCount(): number;
-    /** @public */
+    /**
+     * Get the index of the last line (0-indexed)
+     *
+     * @returns The index of the last line
+     *
+     * @example
+     * ```ts
+     * console.log(editor.lastLine());
+     * ```
+     * @public */
     abstract lastLine(): number;
-    /** @public */
+    /**
+     * Get the selection
+     *
+     * @returns The selection
+     *
+     * @example
+     * ```ts
+     * console.log(editor.getSelection());
+     * ```
+     *
+     * @public */
     abstract getSelection(): string;
-    /** @public */
+    /**
+     * Check if there is a selection
+     *
+     * @returns Whether there is a selection
+     *
+     * @example
+     * ```ts
+     * console.log(editor.somethingSelected());
+     * ```
+     *
+     * @public
+     */
     somethingSelected(): boolean;
-    /** @public */
+    /**
+     * Get the range between two positions
+     *
+     * @param from - The start position
+     * @param to - The end position
+     * @returns The range between the two positions
+     *
+     * @example
+     * ```ts
+     * console.log(editor.getRange(from, to));
+     * ```
+     *
+     * @public
+     * */
     abstract getRange(from: EditorPosition, to: EditorPosition): string;
-    /** @public */
-    abstract replaceSelection(replacement: string, origin?: string): void;
-    /** @public */
+    /**
+     * Replace the range between two positions
+     *
+     * @param replacement - The replacement text
+     * @param from - The start position
+     * @param to - The end position
+     * @param origin - The user event that triggered the replacement
+     *
+     * @example
+     * ```ts
+     * editor.replaceRange('foo', from, to);
+     * ```
+     *
+     * @public
+     **/
     abstract replaceRange(replacement: string, from: EditorPosition, to?: EditorPosition, origin?: string): void;
-    /** @public */
+    /**
+     * Get the cursor position
+     *
+     * @param string - The type of cursor to get
+     * @returns The cursor position
+     *
+     * @example
+     * ```ts
+     * console.log(editor.getCursor('from'));
+     * ```
+     * @public
+     **/
     abstract getCursor(string?: 'from' | 'to' | 'head' | 'anchor'): EditorPosition;
-    /** @public */
+    /**
+     * Get the list of selections if multiple cursors are active
+     *
+     * @returns The list of selections
+     *
+     * @example
+     * ```ts
+     * console.log(editor.listSelections());
+     * ```
+     *
+     * @public
+     **/
     abstract listSelections(): EditorSelection[];
-    /** @public */
+    /**
+     * Set the cursor position
+     *
+     * @param pos - The position to set the cursor to
+     * @param ch - The character index to set the cursor to (0-based)
+     *
+     * @example
+     * ```ts
+     * editor.setCursor({ line: 12, ch: 3 });
+     * editor.setCursor(12, 3);
+     * ```
+     *
+     * @public
+     **/
     setCursor(pos: EditorPosition | number, ch?: number): void;
-    /** @public */
+    /**
+     * Set the selection
+     *
+     * @param anchor - The start selection position
+     * @param head - The end selection position
+     *
+     * @example
+     * ```ts
+     * editor.setSelection({ line: 12, ch: 3 }, { line: 23, ch: 4 });
+     * ```
+     *
+     * @public
+     **/
     abstract setSelection(anchor: EditorPosition, head?: EditorPosition): void;
-    /** @public */
+    /**
+     * Set the selections
+     *
+     * @param ranges - The ranges to set the selections to
+     * @param main - The main selection index
+     *
+     * @example
+     * ```ts
+     * editor.setSelections([
+     *     { anchor: { line: 12, ch: 3 }, head: { line: 23, ch: 4 } },
+     *     { anchor: { line: 34, ch: 5 }, head: { line: 45, ch: 6 } }
+     * ], 1);
+     * ```
+     *
+     * @public
+     **/
     abstract setSelections(ranges: EditorSelectionOrCaret[], main?: number): void;
-    /** @public */
+    /**
+     * Focus the editor
+     *
+     * @example
+     * ```ts
+     * editor.focus();
+     * ```
+     *
+     * @public
+     **/
     abstract focus(): void;
-    /** @public */
+    /**
+     * Blur the editor
+     *
+     * @example
+     * ```ts
+     * editor.blur();
+     * ```
+     *
+     * @public
+     **/
     abstract blur(): void;
-    /** @public */
+    /**
+     * Check if the editor is focused
+     *
+     * @returns Whether the editor is focused
+     *
+     * @example
+     * ```ts
+     * console.log(editor.hasFocus());
+     * ```
+     *
+     * @public
+     **/
     abstract hasFocus(): boolean;
-    /** @public */
+    /**
+     * Get the scroll info (horizontal and vertical scroll positions)
+     *
+     * @returns The scroll info
+     *
+     * @example
+     * ```ts
+     * console.log(editor.getScrollInfo());
+     * ```
+     *
+     * @public
+     **/
     abstract getScrollInfo(): {
         /** @public */
         top: number;
         /** @public */
         left: number;
     };
-    /** @public */
+    /**
+     * Scroll to a specific position
+     *
+     * @param x - The horizontal scroll position
+     * @param y - The vertical scroll position
+     *
+     * @example
+     * ```ts
+     * editor.scrollTo(12, 34);
+     * ```
+     *
+     * @public
+     **/
     abstract scrollTo(x?: number | null, y?: number | null): void;
-    /** @public */
+    /**
+     * Scroll into view
+     *
+     * @param range - The range to scroll into view
+     * @param center - Whether to center the range
+     *
+     * @example
+     * ```ts
+     * editor.scrollIntoView({ from: { line: 12, ch: 3 }, to: { line: 23, ch: 4 } }, true);
+     * ```
+     *
+     * @public
+     **/
     abstract scrollIntoView(range: EditorRange, center?: boolean): void;
-    /** @public */
+    /**
+     * Undo the last action
+     *
+     * @example
+     * ```ts
+     * editor.undo();
+     * ```
+     *
+     * @public
+     **/
     abstract undo(): void;
-    /** @public */
+    /**
+     * Redo the last action
+     *
+     * @example
+     * ```ts
+     * editor.redo();
+     * ```
+     *
+     * @public
+     **/
     abstract redo(): void;
-    /** @public */
+    /**
+     * Execute a command
+     *
+     * @param command - The command to execute
+     *
+     * @example
+     * ```ts
+     * editor.exec('goUp');
+     * ```
+     *
+     * @public
+     **/
     abstract exec(command: EditorCommandName): void;
-    /** @public */
-    abstract transaction(tx: EditorTransaction, origin?: string): void;
-    /** @public */
+    /**
+     * Get the word at a specific position
+     *
+     * @param pos - The position to get the word at
+     * @returns A range object containing the word or `null` if there is no word at the position
+     *
+     * @example
+     * ```ts
+     * console.log(editor.wordAt({ line: 12, ch: 3 }));
+     * ```
+     *
+     * @public
+     **/
     abstract wordAt(pos: EditorPosition): EditorRange | null;
-    /** @public */
+    /**
+     * Convert a position to an offset
+     *
+     * @param pos - The position to convert
+     * @returns The offset
+     *
+     * @example
+     * ```ts
+     * console.log(editor.posToOffset({ line: 12, ch: 3 }));
+     * ```
+     *
+     * @public
+     **/
     abstract posToOffset(pos: EditorPosition): number;
-    /** @public */
+    /**
+     * Convert an offset to a position
+     *
+     * @param offset - The offset to convert
+     * @returns The position
+     *
+     * @example
+     * ```ts
+     * console.log(editor.offsetToPos(123));
+     * ```
+     *
+     * @public
+     **/
     abstract offsetToPos(offset: number): EditorPosition;
 
-    /** @public */
+    /**
+     * Process lines
+     *
+     * @typeParam T - The type of the value to process
+     * @param read - The function to convert the line to a value
+     * @param write - The function to convert the line with a value to the editor change
+     * @param ignoreEmpty - Whether to ignore empty lines
+     *
+     * @example
+     * ```ts
+     * editor.processLines((line, lineText) => {
+     *     return lineText.length;
+     * }, (line, lineText, value) => {
+     *     return { text: line + value, from: { line, ch: 0 }, to: { line, ch: lineText.length } };
+     * }, true);
+     * ```
+     *
+     * @public
+     **/
     processLines<T>(read: (line: number, lineText: string) => T | null, write: (line: number, lineText: string, value: T | null) => EditorChange | void, ignoreEmpty?: boolean): void;
 
 }
