6365160593283c2becb044716c28fbab6854fe66
Michael Naumov <mnaoumov@gmail.com>
2025-03-12 18:53:30 -0600

    NumberConstructor

--- obsidian.d.ts
+++ obsidian.d.ts
@@ -275,7 +275,27 @@ declare global {
          */
         format(...args: string[]): string;
     }
+    /**
+     * Augments the built-in `Number` constructor.
+     */
     interface NumberConstructor {
+        /**
+         * Type guard to check if a value is a number.
+         *
+         * @param obj - The value to check.
+         * @returns `true` if the value is a number, `false` otherwise.
+         *
+         * @example
+         * ```ts
+         * console.log(Number.isNumber(123)); // true
+         * console.log(Number.isNumber('123')); // false
+         * console.log(Number.isNumber(NaN)); // true
+         * console.log(Number.isNumber(Infinity)); // true
+         * console.log(Number.isNumber(-Infinity)); // true
+         * ```
+         *
+         * @remarks Regarding `NaN` see: {@link https://forum.obsidian.md/t/bug-number-isnumber-definition/98104}
+         */
         isNumber(obj: any): obj is number;
     }
     interface Node {
