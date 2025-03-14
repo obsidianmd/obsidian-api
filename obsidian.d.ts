ed54a8066820f7af14df1ec966fb91e9e4f0abd1
Michael Naumov <mnaoumov@gmail.com>
2025-03-14 11:43:17 -0600

    htmlToMarkdown

--- obsidian.d.ts
+++ obsidian.d.ts
@@ -6009,6 +6009,18 @@ export interface HSL {
 
 /**
  * Converts HTML to a Markdown string.
+ *
+ * @example
+ * ```ts
+ * console.log(htmlToMarkdown('<h1>foo</h1>')); // # foo
+ * const el = createDiv();
+ * el.createEl('h2', { text: 'bar' });
+ * console.log(htmlToMarkdown(el)); // ## bar
+ * const fragment = createFragment();
+ * fragment.createEl('h3', { text: 'baz' });
+ * console.log(htmlToMarkdown(fragment)); // ### baz
+ * ```
+ *
  * @public
  */
 export function htmlToMarkdown(html: string | HTMLElement | Document | DocumentFragment): string;
