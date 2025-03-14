af634c13f1589d0e3c2803c96be9044632fbe762
Michael Naumov <mnaoumov@gmail.com>
2025-03-14 13:50:02 -0600

    Spaces

--- obsidian.d.ts
+++ obsidian.d.ts
@@ -6176,11 +6176,11 @@ export function iterateCacheRefs(cache: CachedMetadata, cb: (ref: ReferenceCache
 
 /**
  * If callback returns true, the iteration process will be interrupted.
- * 
+ *
  * @param refs - The references to iterate.
  * @param cb - The callback to call for each reference.
  * @returns true if callback ever returns true, false otherwise.
- * 
+ *
  * @example
  * ```ts
  * iterateRefs(refs, (ref) => {
