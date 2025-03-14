8133c05a8692c158075dfa28c71b8d9c0e5bb2ce
Michael Naumov <mnaoumov@gmail.com>
2025-03-14 11:53:55 -0600

    ISuggestOwner

--- obsidian.d.ts
+++ obsidian.d.ts
@@ -6061,16 +6061,44 @@ export interface Instruction {
 }
 
 /**
+ * An owner that controls UI suggestions.
+ * 
+ * @typeParam T - The type of the suggestion.
+ *
  * @public
  */
 export interface ISuggestOwner<T> {
     /**
      * Render the suggestion item into DOM.
+     * 
+     * @param value - The value of the suggestion.
+     * @param el - The DOM element to render the suggestion into.
+     * 
+     * @example
+     * ```ts
+     * class MySuggestOwner implements ISuggestOwner<string> {
+     *     public renderSuggestion(value: string, el: HTMLElement): void {
+     *         el.createEl('div', { text: value });
+     *     }
+     * }
+     * ```
      * @public
      */
     renderSuggestion(value: T, el: HTMLElement): void;
     /**
      * Called when the user makes a selection.
+     * 
+     * @param value - The value of the suggestion.
+     * @param evt - The event that triggered the selection.
+     * 
+     * @example
+     * ```ts
+     * class MySuggestOwner implements ISuggestOwner<string> {
+     *     public selectSuggestion(value: string, evt: MouseEvent | KeyboardEvent): void {
+     *         console.log(value, evt);
+     *     }
+     * }
+     * ```
      * @public
      */
     selectSuggestion(value: T, evt: MouseEvent | KeyboardEvent): void;
